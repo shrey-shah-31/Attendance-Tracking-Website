c81091ead5be6f12b834318e020f3ba05f72e370
shrey-shah-31 <shreyshah2005@gmail.com>
2026-04-05 21:56:06 +0530

    Add files via upload

---
+++ setcookie.php
@@ -0,0 +1,9 @@
+<?php
+setcookie("name", $_POST['name']);
+setcookie("surname", $_POST['surname']);
+setcookie("email", $_POST['email']);
+setcookie("phone", $_POST['phone']);
+setcookie("city", $_POST['city']);
+?>
+
+<a href="show.php">Show Details</a>
